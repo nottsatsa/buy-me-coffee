0eb2ba79d02f13e86d802d173ab8dc2a09aca00f
24LP6774 24LP6774 <24LP6774@G305-employeeiMac.local>
2025-05-08 09:56:27 +0800

    username comp done

--- front/src/app/(auth)/sign-up/page.tsx
+++ front/src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from '../_components/header';
+import { UsernameInput } from '../_components/usernameInput';
 
 export default function Home() {
   return (
     <div className="h-100vh">
       sign up
       <Header noName={true} username="" />
+      <UsernameInput />
     </div>
   );
 }
