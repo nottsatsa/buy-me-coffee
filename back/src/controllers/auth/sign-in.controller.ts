b1d9e6c3f2d772c5179dde8211b24f389a08d8f3
24LP6774 24LP6774 <24LP6774@G305-employeeiMac.local>
2025-05-12 12:18:03 +0800

    token avdag bolson

--- back/src/controllers/auth/sign-in.controller.ts
+++ back/src/controllers/auth/sign-in.controller.ts
@@ -1,30 +1,39 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../utils/prisma';
-import bcrypt, { compareSync } from 'bcrypt';
+import bcrypt, { compare, compareSync } from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { secret_key } from '../../utils/env';
 
 export const signIn = async (req: Request, res: Response) => {
-  //   const { email, password } = req.body;
-  const email = 'hud@gmail.com';
-  const password = '1234';
-  const hashedPassword = await bcrypt.hashSync(password, 10);
-  console.log(hashedPassword, 'hashedPas');
+  const { email, password } = req.body;
+  // const email = 'hud@gmail.com';
+  // const password = '1234';
+  // const hashedPassword = await bcrypt.hashSync(password, 10);
+  // console.log(hashedPassword, 'hashedPas');
+  console.log('working...');
+
   try {
     const user = await prisma.user.findUnique({
       where: { email: email },
     });
+    console.log('working user...', user);
+
     if (!user) return res.send({ message: 'user not found' });
 
-    const isMatch = compareSync(password, user.password);
+    const isMatch = await compare(password, user.password);
+
     if (!isMatch) return res.send({ message: 'email or pass wrong' });
 
     const token = jwt.sign(user, secret_key as any, { expiresIn: 3600 });
-
-    return res.cookie('token', token, {
-      maxAge: 60 * 1000 * 10,
-      secure: false, //deploydoh uedee true bolgono
+    console.log('working token...', token);
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: false, // Set to true in production
+      sameSite: 'lax',
+      // domain: '.example.com', // Optional
+      path: '/',
     });
+    return res.send({ message: 'success', token: token });
   } catch (error) {
     return res.send(error);
   }
--- back/src/controllers/auth/sign-up.controller.ts
+++ back/src/controllers/auth/sign-up.controller.ts
@@ -1,31 +1,32 @@
-import { Request, Response } from 'express';
-import { prisma } from '../../utils/prisma';
-import bcrypt from 'bcrypt';
+// import { Request, Response } from 'express';
+// import { prisma } from '../../utils/prisma';
+// import bcrypt from 'bcrypt';
 
-export const checkUser = (req: Request, res: Response) => {
-  const { username } = req.body;
-  try {
-    const user = prisma.user.findUnique({ where: { name: username } });
-    if (!user) {
-      return res.send({ success: true, message: response });
-    }
-  } catch (error) {}
-};
+// export const checkUser = async (req: Request, res: Response) => {
+//   const { username } = req.body;
+//   try {
+//     const userUsername = await prisma.user.findUnique({ where: { name: username }, });
+//     if (userUsername) {
+//       return res.send({ message: "Username already taken" });
+//     }
+//     return res.send({message: "Username already taken" });
+//   } catch (error) {}
+// };
 
-export const signUp = async (req: Request, res: Response) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hashSync(password, 10);
-  console.log(hashedPassword, 'hashedPas');
-  try {
-    const response = await prisma.user.create({
-      data: {
-        password: hashedPassword,
-        email,
-        name: username,
-      },
-    });
-    return res.send({ success: true, message: response });
-  } catch (error) {
-    return res.send(error);
-  }
-};
+// export const signUp = async (req: Request, res: Response) => {
+//   const { username, email, password } = req.body;
+//   const hashedPassword = await bcrypt.hashSync(password, 10);
+//   console.log(hashedPassword, 'hashedPas');
+//   try {
+//     const response = await prisma.user.create({
+//       data: {
+//         password: hashedPassword,
+//         email,
+//         name: username,
+//       },
+//     });
+//     return res.send({ success: true, message: response });
+//   } catch (error) {
+//     return res.send(error);
+//   }
+// };
