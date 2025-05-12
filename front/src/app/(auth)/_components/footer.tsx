ad1c1417a9510e7479b9d5faef4a51a20b43c316
24LP6774 24LP6774 <24LP6774@G305-employeeiMac.local>
2025-05-12 11:14:10 +0800

    formuud shalgadag bolov

--- front/src/app/(auth)/_components/footer.tsx
+++ front/src/app/(auth)/_components/footer.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/ui/button';
 
-export const FooterBtn = ({ className }: any) => {
+export const FooterBtn = ({ className, type }: any) => {
   return (
     <div className="h-[64px] w-[407px] p-6 pt-0">
-      <Button className={`bg-[#18181B] h-10 w-[359px] ${className}`}>
+      <Button
+        type={type}
+        className={`bg-[#18181B] h-10 w-[359px] ${className}`}
+      >
         Continue
       </Button>
     </div>
